f4dcb80c15afe5b8a3bc09ce1c03befe732d16db
Akaki Dzidziguri <adzid23@freeuni.edu.ge>
2025-11-18 20:00:04 +0400

    added header for my_strncpy

--- include/my_helpers.h
+++ include/my_helpers.h
@@ -10,6 +10,7 @@ int my_strncmp(const char *str1, const char *str2, size_t n);
 char *my_getenv(char *variable_name, char **env);
 char *my_strdup(const char *str);
 char *my_strcpy(char *dest, const char *src);
+char *my_strncpy(char *dest, const char *src, size_t n);
 char *my_strchr(const char *str, char c);
 char *my_strtok(char *input, const char *delim);
 
